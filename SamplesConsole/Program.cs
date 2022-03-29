1ec89d8bcd6cf9fc5804c77ae5c5040e91a0d9d9
meenakshikumar <meenakshi.kumar@bold.com>
2022-03-29 16:01:02 +0530

    Adding comments and updating Program.cs

--- SamplesConsole/Program.cs
+++ SamplesConsole/Program.cs
@@ -8,19 +8,25 @@ public class Program
 
     static void Main(string[] args)
     {
+        /*
+         * Quick Run: Uncomment individual/multiple lines below to run selective tests
+         */
         //var res = BenchmarkRunner.Run<StringBenchmarks>();
-        //var resDict = BenchmarkRunner.Run<DictionaryBenchmarks>();
+        //var resDict = BenchmarkRunner.Run<DictionaryThreadSafeWriteBenchmarks>();
         //var resAsyncParams = BenchmarkRunner.Run<AsyncParametersBenchmark>();
         //var resListEnumeration = BenchmarkRunner.Run<ListIndexerBenchmark>();
-        var resParallelForVsTask = BenchmarkRunner.Run<ParallelForVsTaskBenchmark>();
+        //var resParallelForVsTask = BenchmarkRunner.Run<ParallelForVsTaskBenchmark>();
 
 
 
-        //BenchmarkRunner.Run(new[]{
-        //    BenchmarkConverter.TypeToBenchmarks( typeof(StringBenchmarks)),
-        //    BenchmarkConverter.TypeToBenchmarks( typeof(DictionaryBenchmarks)),
-        //    BenchmarkConverter.TypeToBenchmarks( typeof(AsyncParametersBenchmark))
-        //    });
+        /*
+         * Run All: Uncomment lines below to run all tests (might take a while to execute)
+         */
+        BenchmarkRunner.Run(new[]{
+            BenchmarkConverter.TypeToBenchmarks( typeof(StringBenchmarks)),
+            BenchmarkConverter.TypeToBenchmarks( typeof(DictionaryThreadSafeWriteBenchmarks)),
+            BenchmarkConverter.TypeToBenchmarks( typeof(AsyncParametersBenchmark))
+            });
     }
 
 }
